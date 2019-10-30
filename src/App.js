8175fecb50f1eed42f61f5076bf5bb8b76a52b2e
Jean Augusto de Caastro Costa <jean.adcc@gmail.com>
2019-10-30 15:07:44 -0300

    Working With React Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./css/webflow.css";
 import "./css/app.css";
@@ -6,63 +6,52 @@ import "./css/app.css";
 import Input from "./components/Input";
 import TaskList from "./components/TaskList";
 
-class App extends React.Component {
-  state = {
-    input: "",
-    items: [
-      { task: "Buy Groceries" },
-      { task: "Talk to Mom" },
-      { task: "Take out cash for trip" }
-    ]
-  };
+function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
 
-  componentDidUpdate() {
-    localStorage.setItem("items", JSON.stringify(this.state.items));
+  useEffect(() => {
+    const localItems = localStorage.getItem("items");
+    if (!localItems) {
+      setItems(
+        { task: "Buy Groceries" },
+        { task: "Talk to Mom" },
+        { task: "Take out cash for trip" }
+      );
+      localStorage.setItem("items", JSON.stringify(items));
+    } else {
+      setItems(JSON.parse(localItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  });
+
+  function handleDelete(itemTask) {
+    const NewArray = items.filter(item => item.task !== itemTask);
+    setItems(NewArray);
   }
 
-  componentDidMount() {
-    var taskList = localStorage.getItem("items");
-    if (taskList) {
-      this.setState({ items: JSON.parse(taskList) });
-    }
-  }
-
-  onChange = event => {
-    this.setState({ input: event.target.value });
-  };
-
-  onSubmit = event => {
-    event.preventDefault();
-    if (!this.state.input) {
-      return;
-    }
-    this.setState({
-      items: [...this.state.items, { task: this.state.input }],
-      input: ""
-    });
-  };
-
-  handleDelete = itemTask => {
-    const items = this.state.items.filter(item => item.task !== itemTask);
-    this.setState({ items: items });
-  };
-
-  render() {
-    const { items } = this.state;
-    return (
-      <div className="body-2">
-        <h1 className="page-title">To-Do List</h1>
-        <div className="content-wrapper">
-          <Input
-            value={this.state.input}
-            onChange={this.onChange}
-            onClick={this.onSubmit}
-          />
-          <TaskList handleDelete={this.handleDelete} items={items} />
-        </div>
+  return (
+    <div className="body-2">
+      <h1 className="page-title">To-Do List</h1>
+      <div className="content-wrapper">
+        <Input
+          value={input}
+          onChange={event => setInput(event.target.value)}
+          onClick={() => {
+            if (!input) {
+              return;
+            }
+            setItems([...items, { task: input }]);
+            setInput("");
+          }}
+        />
+        <TaskList handleDelete={handleDelete} items={items} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,42 +1,38 @@
 import React from "react";
 
-class Input extends React.Component {
-  onKeyPress = event => {
+function Input({ value, onChange, onClick }) {
+  function onKeyPress(event) {
     if (event.key === "Enter") {
       document.getElementById("SubmitTask").click();
     }
-  };
-
-  render() {
-    const { value, onChange, onClick } = this.props;
-    return (
-      <>
-        <div className="add-item-wrapper">
-          <input
-            type="text"
-            className="to-do-input w-input"
-            maxLength="256"
-            name="New"
-            data-name="New"
-            placeholder="New Task"
-            id="NewTask"
-            required=""
-            value={value}
-            onChange={onChange}
-            onKeyPress={this.onKeyPress}
-          />
-          <a
-            href="#"
-            id="SubmitTask"
-            className="button w-button"
-            onClick={onClick}
-          >
-            +
-          </a>
-        </div>
-      </>
-    );
   }
+  return (
+    <>
+      <div className="add-item-wrapper">
+        <input
+          type="text"
+          className="to-do-input w-input"
+          maxLength="256"
+          name="New"
+          data-name="New"
+          placeholder="New Task"
+          id="NewTask"
+          required=""
+          value={value}
+          onChange={onChange}
+          onKeyPress={() => onKeyPress(event)}
+        />
+        <a
+          href="#"
+          id="SubmitTask"
+          className="button w-button"
+          onClick={onClick}
+        >
+          +
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Input;
